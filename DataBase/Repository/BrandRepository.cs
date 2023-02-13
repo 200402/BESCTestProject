87d92287cddff40008f2608ecad8b83798a9ed19
200402 <r_he_lol@mail.ru>
2023-02-13 18:24:08 +0500

    подключение бд

---
+++ DataBase/Repository/BrandRepository.cs
@@ -0,0 +1,25 @@
+﻿using DataBase.Interfaces;
+using DataBase.Tables;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase.Repository
+{
+    public class BrandRepository : IBrand
+    {
+        private readonly ApplicationContext context;
+        private readonly IMemoryCache _memoryCache;
+
+
+        public BrandRepository(ApplicationContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Brand> AllBrands => context.Brands.ToList();
+    }
+}
---
+++ DataBase/Repository/ModelRepository.cs
@@ -0,0 +1,23 @@
+﻿using DataBase.Interfaces;
+using DataBase.Tables;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase.Repository
+{
+    public class ModelRepository : IModel
+    {
+        private readonly ApplicationContext context;
+
+        public ModelRepository(ApplicationContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Model> AllModels => context.Models.ToList();
+    }
+}
